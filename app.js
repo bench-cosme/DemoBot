af1d80f58867a41586991c44ad4b33504b91c4f1
Cielito <cosmebench@gmail.com>
2017-11-03 19:42:03 +0800

    push

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: "805a15fb-5eee-48eb-9dbc-dff07b233c1c",
+    appPassword: "vuocpMC45>}ibPXSOJ967(_"
 });
 
 // Listen for messages from users 
